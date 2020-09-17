19741ad1cb36a5581a907f10a734e96d2b2a73b3
Nerbyk <danil.maximov2000@gmail.com>
2020-09-17 17:24:10 -0400

    changed require state

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -5,8 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 require 'telegram/bot'
 require 'dotenv'
 
-require './responder/responder.rb'
-require './responder/admin_responder.rb'
+require './telegram/responder/responder.rb'
+require './telegram/responder/admin_responder.rb'
 
 token = ENV['TELEGRAM_TOKEN']
 
