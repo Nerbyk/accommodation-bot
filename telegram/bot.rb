8626424248fcccac52f8e4431323e6b2344f70ff
Nerbyk <danil.maximov2000@gmail.com>
2020-09-17 17:59:54 -0400

    error handling, avoiding bot shutdown due error

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -10,14 +10,37 @@ require './telegram/responder/admin_responder.rb'
 
 token = ENV['TELEGRAM_TOKEN']
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    if message.from.id == ENV['ADMIN_ID'].to_i
-      AdminResponder.new(message, bot)
-    else
-      Responder.new(message, bot)
+# Telegram::Bot::Client.run(token) do |bot|
+#   bot.listen do |message|
+#     if message.from.id == ENV['ADMIN_ID'].to_i
+#       AdminResponder.new(message, bot)
+#     else
+#       Responder.new(message, bot)
+#     end
+#   rescue StandardError => e
+#     bot.api.send_message(chat_id: message.from.id, text: "error: #{e}")
+#   end
+# end
+
+
+loop do
+  begin
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        Thread.start(message) do |rqst|
+          begin
+            if message.from.id == ENV['ADMIN_ID'].to_i
+              AdminResponder.new(message, bot)
+            else
+              Responder.new(message, bot)
+            end
+          rescue StandardError => e
+            bot.api.send_message(chat_id: message.from.id, text: "error: #{e}")
+          end
+        end
+      end
     end
-  rescue StandardError => e
-    bot.api.send_message(chat_id: message.from.id, text: "error: #{e}")
-  end
+  rescue StandardError => low_e 
+    bot.api.send_message(chat_id: ENV['ADMIN_ID'].to_i, text: "LOW error from #{message.from.id}: #{low}")
+  end 
 end
