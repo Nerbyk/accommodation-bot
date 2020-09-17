3e7f440ad0aa61723c946c09b5bfc3eda094563c
Nerbyk <danil.maximov2000@gmail.com>
2020-09-17 16:50:43 -0400

    new rendering for all

--- telegram/responder/responder.rb
+++ telegram/responder/responder.rb
@@ -8,53 +8,112 @@ class Responder
     menu
   end
 
-  private
+  protected
 
   def access_denied
     bot.api.send_message(chat_id: message.from.id, text: 'Your account is not activated for now')
   end
-  
+
   def check_search_case(string)
     return 'surname' if string.first == '_'
     return 'name' if !!string.capitalize.match(/^[a-zA-Z]*$/)
     return 'name_surname' if !!string.capitalize.match(/^[a-zA-Z]{0,19}[\s,][a-zA-Z]{0,19}$/)
-    return 'invalid'
-  end 
+    return 'floor' if !!string.match(/^[A-Ba-b]{1}0[1-7]{1}-[1-9]{1}$/)
 
-  def render(request)
-      request.each do |student|
-        bot.api.send_message(chat_id: message.from.id, text: "Block: " + student.block + "\n"+
-                                                             "Room: " + student.room.to_s + "\n" +
-                                                             "Name Surname: " + student.name + " " + student.surname )
-      end 
+    'invalid'
+  end
 
+  def find_neighbor(request)
+    searched_student = { name: request.name, surname: request.surname }
+    neighbors = Student.where(room: request.room, block: request.block)
+    neighbors = neighbors.map do |student|
+      if student.name == searched_student[:name] && student.surname == searched_student[:surname]
+        nil
+      else
+        student
+      end
+    end
+    neighbors = neighbors.compact.map do |neighbor|
+      neighbor.name + ' ' + neighbor.surname
+    end
+    neighbors.join(',')
+  end
 
-  end 
+  def render_person(request)
+    rendered_msg = ''
+    request.each_with_index do |student, i|
+      neighbors = find_neighbor(student)
+      neighbors = 'None' if neighbors.empty?
+      rendered_msg += 'Block: ' + student.block + "\n" \
+                     'Room: ' + student.room.to_s + "\n" \
+                     'Name Surname: ' + student.name + ' ' + student.surname + "\n" \
+                     'Neighbors: ' + neighbors + "\n================" + "\n\n"
+
+      if (i % 7).zero? && i != 0
+        bot.api.send_message(chat_id: message.from.id, text: rendered_msg)
+        rendered_msg = ''
+      elsif request.length - 1 == i
+        bot.api.send_message(chat_id: message.from.id, text: rendered_msg)
+      end
+    end
+  end
 
   def find_student(string)
-      search_str = string
-      search_by = check_search_case(string)
-      case search_by 
-      when 'name'
-        request = Student.where(name: search_str)
-      when 'surname'
-        search_str[0] = ''
-        request = Student.where(surname: search_str.capitalize)
-      when 'name_surname'
-        name, surname = search_str.split(' ')
-        request = Student.where(name: name, surname: surname)
-      else  
-        raise 'Invalid Input'
-      end 
+    search_str = string
+    search_by = check_search_case(string)
+    case search_by
+    when 'name'
+      request = Student.where(name: search_str)
+    when 'surname'
+      search_str[0] = ''
+      request = Student.where(surname: search_str.capitalize)
+    when 'name_surname'
+      name, surname = search_str.split(' ')
+      request = Student.where(name: name, surname: surname)
+    when 'floor'
+      block, floor = string.split('-')
+      request = Student.where(block: block, floor: floor.to_i)
+      request = request.each_with_index.map do |student, i|
+        if i != 0
+          student.room == request[i - 1].room ? nil : student
+        end
+      end
+      request.compact!
+    else
+      raise 'Invalid Input'
+    end
 
-      render request
-      
+    render_person request
   end
 
   def validate
     user = User.find_by(telegram_id: message.from.id)
-    user = User.create(telegram_id: message.from.id, step: 'new', access: false) if user.nil?
+    if user.nil?
+      user = User.create(telegram_id: message.from.id, step: 'new', access: false)
+      bot.api.send_message(chat_id: ENV['ADMIN_ID'], text: "New User Joined Bot.\n\n/accept_#{message.from.id} or /deny_#{message.from.id}")
+    end
     user
+  end
+
+  def updated_at
+    Student.first.updated_at.strftime('%a %d %b %Y').to_s
+  end
+
+  def available_blocks
+    Student.distinct.pluck(:block).join(', ')
+  end
+
+  def start_message_text
+    "<b>Last Update:</b> #{updated_at}\n<b>Available Blocks:</b> #{available_blocks}\n\n<b>Usage:</b>\n\n" \
+      "Enter student <i>Name</i> to search by name(e.g. Bob)\n\n" \
+      "Enter student <i>_Surname</i> to search by surname(e.g. _Black)\n\n" \
+      "Enter <i>Name Surname</i> to search by name and surname(e.g. Bob Black)\n\n" \
+      'Enter <i>Block-Floor</i> to get a list for the entire floor(e.g. A03-7)'
+  end
+
+  def start_message
+    bot.api.send_message(chat_id: message.from.id, text: start_message_text, parse_mode: 'HTML')
+    p available_blocks
   end
 
   def menu
@@ -62,12 +121,12 @@ class Responder
       access_denied
       return
     end
-    
+
     if message.text == '/start'
-      bot.api.send_message(chat_id: message.from.id, text: "Для поиска по имени введи просто имя\n\nДля поиска по фамилии введи знак _перед фамилией (_Ivanov)\n\nДля Поиска по имени и фамилию введи имя и фамилию(Name Surname)")
-    else 
+      start_message
+    else
       find_student(message.text)
-    end  
+    end
   end
 
   attr_accessor :bot, :message, :user
