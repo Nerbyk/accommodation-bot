5b179e1b8ebd03012f045dc8b2cd01d933d628b3
Nerbyk <danil.maximov2000@gmail.com>
2020-09-19 21:08:34 -0400

    typeerror resolved

--- telegram/responder/admin_responder.rb
+++ telegram/responder/admin_responder.rb
@@ -19,7 +19,7 @@ class AdminResponder < Responder
     users = User.all
         bot_users = users.map do 
           |user| 
-          return "User: <a href=\"tg://user?id=#{user.telegram_id}\">#{user.telegram_id.to_s}</a> Registrated: #{user.updated_at.strftime('%a %d %b %Y').to_s}\n\n"
+          return "User: <a href=\"tg://user?id=#{user.telegram_id.to_s}\">#{user.telegram_id.to_s}</a> Registrated: #{user.updated_at.strftime('%a %d %b %Y').to_s}\n\n"
         end 
         bot.api.send_message(chat_id: message.from.id, text: bot_users, parse_mode: "HTML")
   end 
