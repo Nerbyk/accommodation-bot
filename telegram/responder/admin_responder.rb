84555897821c1439574d62f3b43e13783aac9599
Nerbyk <danil.maximov2000@gmail.com>
2020-09-20 18:03:08 -0400

    users last user input

--- telegram/responder/admin_responder.rb
+++ telegram/responder/admin_responder.rb
@@ -20,7 +20,7 @@ class AdminResponder < Responder
         bot_users = ''
         users.each do 
           |user| 
-          bot_users += "User: <a href=\"tg://user?id=#{user.telegram_id.to_s}\">#{user.telegram_id.to_s}</a> Registrated: #{user.updated_at.strftime('%a %d %b %Y').to_s}\n\n"
+          bot_users += "User: <a href=\"tg://user?id=#{user.telegram_id.to_s}\">#{user.telegram_id.to_s}</a> Registrated: #{user.updated_at.strftime('%a %d %b %Y').to_s} | last request: #{user.step}\n\n"
         end 
         bot.api.send_message(chat_id: message.from.id, text: bot_users, parse_mode: "HTML")
   end 
